b7dc7fcfa62048e0374a0aa77d3fac1a13a4abf4
xNtrow <xntrowdev@gmail.com>
2022-02-21 17:47:22 +0100

    Fixed Some Errors

--- src/main/java/org/xnotro/signtask/Core.java
+++ src/main/java/org/xnotro/signtask/Core.java
@@ -13,6 +13,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -23,54 +24,53 @@ public class Core extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        logger.info("[SignTask] > Plugin Enabled.");
+        logger.info( "[SignTask] > Plugin Enabled." );
         saveDefaultConfig();
-        Bukkit.getPluginManager().registerEvents(this, this);
+        Bukkit.getPluginManager().registerEvents( this, this );
     }
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        Block block = event.getClickedBlock();
-        BlockState blockState = block.getState();
 
-        String reward1 = getConfig().getString("rewards.reward1");
-        String reward2 = getConfig().getString("rewards.reward2");
-        String reward3 = getConfig().getString("rewards.reward3");
-        String reward4 = getConfig().getString("rewards.reward4");
-        String reward5 = getConfig().getString("rewards.reward5");
+        String reward1 = getConfig().getString( "rewards.reward1" );
+        String reward2 = getConfig().getString( "rewards.reward2" );
+        String reward3 = getConfig().getString( "rewards.reward3" );
+        String reward4 = getConfig().getString( "rewards.reward4" );
+        String reward5 = getConfig().getString( "rewards.reward5" );
 
-        String signText1 = getConfig().getString("rewards.signText1");
-        String signText2 = getConfig().getString("rewards.signText2");
-        String signText3 = getConfig().getString("rewards.signText3");
-        String signText4 = getConfig().getString("rewards.signText4");
-        String signText5 = getConfig().getString("rewards.signText5");
+        String signText1 = getConfig().getString( "rewards.signText1" );
+        String signText2 = getConfig().getString( "rewards.signText2" );
+        String signText3 = getConfig().getString( "rewards.signText3" );
+        String signText4 = getConfig().getString( "rewards.signText4" );
+        String signText5 = getConfig().getString( "rewards.signText5" );
 
-
-        if (blockState instanceof Sign) {
-            if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                Sign sign = (Sign) blockState;
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (event.getClickedBlock().getState() instanceof Sign) {
+                Sign sign = (Sign) event.getClickedBlock().getState();
                 PlainTextComponentSerializer serializer = PlainTextComponentSerializer.plainText();
                 List<String> signLines = sign.lines().stream().map( serializer :: serialize ).toList();
-                if (player.hasPermission("signtask.reward")) {
-                    if (signLines.get(0).equalsIgnoreCase(signText1)) {
-                        ItemStack re1 = new ItemStack(Material.valueOf(reward1.toUpperCase()));
-                        player.getInventory().addItem(re1);
-                    } else if (signLines.get(0).equalsIgnoreCase(signText2)) {
-                        ItemStack re2 = new ItemStack(Material.valueOf(reward2.toUpperCase()));
-                        player.getInventory().addItem(re2);
-                    } else if (signLines.get(0).equalsIgnoreCase(signText3)) {
-                        ItemStack re3 = new ItemStack(Material.valueOf(reward3.toUpperCase()));
-                        player.getInventory().addItem(re3);
-                    } else if (signLines.get(0).equalsIgnoreCase(signText4)) {
-                        ItemStack re4 = new ItemStack(Material.valueOf(reward4.toUpperCase()));
-                        player.getInventory().addItem(re4);
-                    } else if (signLines.get(0).equalsIgnoreCase(signText5)) {
-                        ItemStack re5 = new ItemStack(Material.valueOf(reward5.toUpperCase()));
-                        player.getInventory().addItem(re5);
+                if (player.hasPermission( "signtask.reward" )) {
+                    if (signLines.get( 0 ).equalsIgnoreCase( signText1 )) {
+                        ItemStack re1 = new ItemStack( Material.valueOf( reward1.toUpperCase() ) );
+                        player.getInventory().addItem( re1 );
+                    } else if (signLines.get( 0 ).equalsIgnoreCase( signText2 )) {
+                        ItemStack re2 = new ItemStack( Material.valueOf( reward2.toUpperCase() ) );
+                        player.getInventory().addItem( re2 );
+                    } else if (signLines.get( 0 ).equalsIgnoreCase( signText3 )) {
+                        ItemStack re3 = new ItemStack( Material.valueOf( reward3.toUpperCase() ) );
+                        player.getInventory().addItem( re3 );
+                    } else if (signLines.get( 0 ).equalsIgnoreCase( signText4 )) {
+                        ItemStack re4 = new ItemStack( Material.valueOf( reward4.toUpperCase() ) );
+                        player.getInventory().addItem( re4 );
+                    } else if (signLines.get( 0 ).equalsIgnoreCase( signText5 )) {
+                        ItemStack re5 = new ItemStack( Material.valueOf( reward5.toUpperCase() ) );
+                        player.getInventory().addItem( re5 );
                     }
                 }
             }
         }
     }
 }
+
+
